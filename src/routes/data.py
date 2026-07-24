388de261a90c3f59a7be4ec3b03e84c1cd0094f7
Youssef El Nagar <nagarnagar7515@gmail.com>
2026-07-25 00:41:07 +0300

    git add .gitignore git commit -m "chore: update gitignore and untrack sensitive/unnecessary files"

--- src/routes/data.py
+++ src/routes/data.py
@@ -1,13 +1,14 @@
 from fastapi import FastAPI, APIRouter, Depends, UploadFile, status
 from fastapi.responses import JSONResponse
 import os
-from helpers.config import get_settings, Settings
-from controllers.DataController import DataController
-from controllers.ProjectController import ProjectController
+from src.helpers.config import get_settings, Settings
+from src.controllers.DataController import DataController
+from src.controllers.ProjectController import ProjectController
+from src.controllers.ProcessController import ProcessController
 import aiofiles
 from models.enums.ResponseEnums import ResponseSignal
-
 import logging
+from .schemes.data import ProcessRequest
 
 logger = logging.getLogger('uvicorn.error')
 
@@ -20,7 +21,6 @@ data_router = APIRouter(
 async def upload_data(project_id: str, file: UploadFile,
                       app_settings: Settings = Depends(get_settings)):
         
-    
     # validate the file properties
     data_controller = DataController()
 
@@ -56,8 +56,37 @@ async def upload_data(project_id: str, file: UploadFile,
         )
 
     return JSONResponse(
+        content={
+            "signal": ResponseSignal.FILE_UPLOAD_SUCCESS.value,
+            "file_id": file_id
+        }
+    )
+
+
+@data_router.post("/process/{project_id}")
+async def process_endpoint(project_id: str, process_request: ProcessRequest):
+
+    file_id = process_request.file_id
+    chunk_size = process_request.chunk_size
+    overlap_size = process_request.overlap_size
+
+    process_controller = ProcessController(project_id=project_id)
+
+    file_content = process_controller.get_file_content(file_id=file_id)
+
+    file_chunks = process_controller.process_file_content(
+        file_content=file_content,
+        file_id=file_id,
+        chunk_size=chunk_size,
+        overlap_size=overlap_size
+    )
+
+    if file_chunks is None or len(file_chunks) == 0:
+        return JSONResponse(
+            status_code=status.HTTP_400_BAD_REQUEST,
             content={
-                "signal": ResponseSignal.FILE_UPLOAD_SUCCESS.value,
-                "file_id": file_id
+                "signal": ResponseSignal.PROCESSING_FAILED.value
             }
         )
+
+    return file_chunks
